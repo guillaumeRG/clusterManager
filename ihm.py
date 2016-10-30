a114c3ef1534665079f56d100fd0f499883c230c
guillaumeRG <rumor.guillaume@gmail.com>
2016-10-30 00:07:13 +0000

    defaut size

--- ihm.py
+++ ihm.py
@@ -16,7 +16,7 @@ class ihm(Tkinter.Tk):
         self.entry = Tkinter.Entry(self)
         self.entry.grid(column=0,row=0,sticky='EW')
 
-        button = Tkinter.Button(self,text=u"Click me !")
+        button = Tkinter.Button(self,text=u"Ok")
         button.grid(column=1,row=0)
 
         label = Tkinter.Label(self,
@@ -24,5 +24,9 @@ class ihm(Tkinter.Tk):
         label.grid(column=0,row=1,columnspan=2,sticky='EW')
         #redimensionnement auto
         self.grid_columnconfigure(0,weight=1)
+        self.geometry("800x600+300+0")
+        #800x600 <== taille de la fenetre le "x" c'est comme xor
+        #+300 <== ou se positionne en x ou essai -300
+        #+0 <=== ou se positionne en y ou essai-0
 
     
