2538db54980d499052dbbab6c0c8f3ef58e79246
guillaumeRG <rumor.guillaume@gmail.com>
2016-11-09 16:56:24 +0000

    ...

--- service.py
+++ service.py
@@ -135,21 +135,4 @@ if __name__ == "__main__":
 
     #sudo python service.py localhost dhcpcd status
     
-    """print '%s: %s'%(tab[0][0], tab[0][1])
-    for i in range(0,len(tab)):
-         print '%s: %s'%(tab[i][0], tab[i][1])
-    
-    node0 = NodeSet()
-    nbNoeud = len(sys.argv)-3
-    print'nbNoeud: %d'%nbNoeud
-
-    for i in range(1,nbNoeud):
-        node0.add(sys.argv[i])
-
-                
-    print'sudo service '+sys.argv[nbNoeud+1]+' '+sys.argv[nbNoeud+2]
-    task_self().run('sudo service '+sys.argv[nbNoeud+1]+' '+sys.argv[nbNoeud+2], nodes=node0)
-
-    for output, nodes in task_self().iter_buffers():
-        for node in nodes:
-            print '%s: %s'%(node, output)"""
+   
